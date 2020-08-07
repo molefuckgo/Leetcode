749703bb0e1fe2f7d4a6fc8aa39a08edb4953361
guowei <guowei@maetimes.com>
2020-08-07 17:27:16 +0800

    相同的🌲&&字符串包含

--- src/problem/count-the-repetitions/count-the-repetitions.go
+++ src/problem/count-the-repetitions/count-the-repetitions.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(getMaxRepetitions("abaacdbac", 3, "adcbd", 1))
+	fmt.Println(getMaxRepetitions2("abaacdbac", 10, "aaaaca", 1))
 }
 
 func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
@@ -42,8 +42,8 @@ func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
 		}
 
 	}
-	// ans存储的是s1包含的s2的数量，考虑的之前的pre_loop和in_loop
-	ans := preLoop[1] + (n1-preLoop[0])/inLoop[0]*inLoop[1]
+	// ans存储的是s1包含的s2的数量，考虑的之前的preLoop和inLoop
+	ans := preLoop[1] + ((n1-preLoop[0])/inLoop[0])*inLoop[1]
 	// s1的末尾还剩下一些s1，我们暴力进行匹配
 	rest := (n1 - preLoop[0]) % inLoop[0]
 	for i := 0; i < rest; i++ {
@@ -57,6 +57,53 @@ func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
 
 		}
 	}
-	// s1包含ans个s2， 那么就包含ans / n2个s2
+	// s1包含ans个s2， 那么就包含ans / n2 个s2
+	return ans / n2
+}
+
+func getMaxRepetitions2(s1 string, n1 int, s2 string, n2 int) int {
+	if n1 == 0 {
+		return 0
+	}
+	s1Cnt, index, s2Cnt := 0, 0, 0
+	recall := make(map[int][]int)
+	var preLoop, inLoop []int
+	for true {
+		s1Cnt++
+		for _, ch := range s1 {
+			if ch == int32(s2[index]) {
+				index++
+				if index == len(s2) {
+					s2Cnt++
+					index = 0
+				}
+			}
+		}
+		if s1Cnt == n1 { // 还没找到循环节s1就用完了
+			return s2Cnt / n2
+		}
+		if _, ok := recall[index]; ok {
+			s1CntPrime, s2CntPrime := recall[index][0], recall[index][1]
+			preLoop = []int{s1CntPrime, s2CntPrime}
+			inLoop = []int{s1Cnt - s1CntPrime, s2Cnt - s2CntPrime}
+			break
+		} else {
+			recall[index] = []int{s1Cnt, s2Cnt}
+		}
+	}
+	ans := preLoop[1] + ((n1-preLoop[0])/inLoop[0])*inLoop[1]
+	rest := (n1 - preLoop[0]) % inLoop[0]
+	for i := 0; i < rest; i++ {
+		for _, ch := range s1 {
+			if ch == int32(s2[index]) {
+				index++
+
+				if index == len(s2) {
+					ans++
+					index = 0
+				}
+			}
+		}
+	}
 	return ans / n2
 }
---
+++ src/problem/same-tree/same-tree.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil || q == nil {
+		if p == nil && q == nil {
+			return true
+		} else {
+			return false
+		}
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	} else {
+		return false
+	}
+}
